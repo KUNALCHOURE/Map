ec79ba84dcbebc536d4484ab1bdf68f7a6b0d7d3
KUNAL CHOURE <chourekunal4@gmail.com>
2025-04-30 22:01:21 +0530

    ui added

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='h-screen text-white  bg-black'>
+        <div className='text-center '>
+          <h1 className='text-4xl font-bold '>Roadmap Generetor</h1>
+              <hr className='mt-2 border-t-4 ' />
+        </div>
+
+        <div className='flex justify-center items-center h-1/2 flex-col gap-5'>
+
+        <h2 >Hey there ! Whats in your mind?</h2>
+        <input type="text" name="" id="" placeholder='chat with AI' className='bg-gray-100 border-4 rounded-xl w-100 h-12 text-black'/>
+        </div>
+    </div>
+  )
+}
