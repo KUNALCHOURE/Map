a80b802081ce3af36573d91e72864bd81fe012a2
KUNAL CHOURE <chourekunal4@gmail.com>
2025-05-02 18:30:08 +0530

    integrate openAI API

--- Backend/chat.js
+++ Backend/chat.js
@@ -1,10 +1,17 @@
 import { response } from "express";
 import OpenAI from "openai";
-const client=new OpenAI();
+import dotenv from 'dotenv';
+dotenv.config();
+const client=new OpenAI(
+    {
+    apiKey: process.env.API_KEY
+    }
+);
 
-const reponse=await client.responses.create({
-    model:"gpt-4.1",
+const responses=await client.responses.create({
+    model:"gpt-4",
     input:"Write a one sentence bedtime story about a unicorn"
 })
 
-console.log(response)
+console.log(responses.output_text);
+
