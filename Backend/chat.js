99d137830d253bd992bdcc2551596e3a8bc3c423
KUNAL CHOURE <chourekunal4@gmail.com>
2025-05-02 18:22:26 +0530

    set up open ai

---
+++ Backend/chat.js
@@ -0,0 +1,10 @@
+import { response } from "express";
+import OpenAI from "openai";
+const client=new OpenAI();
+
+const reponse=await client.responses.create({
+    model:"gpt-4.1",
+    input:"Write a one sentence bedtime story about a unicorn"
+})
+
+console.log(response)
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
 
 const app=express();
+dotenv.config();
+
 
 app.get("/",(req,res)=>{
 res.send("hello ");
