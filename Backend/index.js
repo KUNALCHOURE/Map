34e072177133690016dc0f0e4d17363b9697aad6
KUNAL CHOURE <chourekunal4@gmail.com>
2025-05-03 19:37:09 +0530

    cors added

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import apiresponse from "./chat.js";
+import cors from 'cors';
 
 const app=express();
 dotenv.config();
+
+app.use(cors({
+   origin:"http://localhost:5173/"
+}));
 
 
 app.get("/",(req,res)=>{
