bac26c99f3f753cba01f409e887f4b7536c61d22
KUNAL CHOURE <chourekunal4@gmail.com>
2025-05-07 22:39:37 +0530

    Back button added

--- Frontend/src/components/Information.jsx
+++ Frontend/src/components/Information.jsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 export default function Information({ title, roadmap }) {
+  const navigate=useNavigate();
+
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center p-6">
+    <div className="min-h-screen bg-black text-white flex items-center justify-center p-6 flex-col">
       <div className="bg-zinc-900 shadow-2xl rounded-2xl p-8 max-w-5xl w-full border border-gray-700">
         <h1 className="text-3xl md:text-4xl font-extrabold text-center mb-6">
           Roadmap for <span className="text-blue-500">{title}</span>
@@ -13,6 +16,10 @@ export default function Information({ title, roadmap }) {
             {roadmap}
           </p>
         </div>
+      </div>
+
+      <div className="border border-blue-300 rounded-xl w-20 text-center mt-7 ">
+        <button className="w-full bg-white text-black rounded-xl" onClick={()=>navigate("/")}>Back</button>
       </div>
     </div>
   );
